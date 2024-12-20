69618c9e496a0aaaeed6020eaeb98b00b9fe6c74
Resnicoff <martin.resnicoff@enel.com>
2024-12-20 20:09:18 -0300

    con build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 2000,
-    outDir: 'build'
-  }
+  outDir: 'dist', 
+base:"https://mresnicoff.github.io/",
 });
